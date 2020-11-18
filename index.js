c9c02b7e4f8176cfa59c1b0cec9dcae0e822e7c8
Renan Cano <uzumaki.renan@hotmail.com>
2020-11-18 17:51:00 -0300

    teste

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ app.post("/add", function(req, res){
 	
 });
 
+var port = process.env.PORT || 3000;
 
-app.listen(3000, function(){
+app.listen(port, function(){
 	console.log("Servidor Rodando.");
 }); 
